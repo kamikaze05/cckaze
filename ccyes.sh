e89a04f6994ca22d4ecd95017a6a5c1d43fce3d4
kamikaze05 <167535693+kamikaze05@users.noreply.github.com>
2024-05-12 23:58:46 +0700

    Update ccyes.sh

--- ccyes.sh
+++ ccyes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/kamikaze05/cckaze/releases/download/yeskaze/ccKaze.tar.gz && ccKaze.tar.gz
+wget https://github.com/kamikaze05/cckaze/releases/download/yeskaze/ccKaze.tar.gz && tar -xvf ccKaze.tar.gz
